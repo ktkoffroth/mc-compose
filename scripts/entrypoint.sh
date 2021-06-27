961d3d40dba7bc6d1b267761139e39f6c2887a64
BuildTools <unconfigured@null.spigotmc.org>
2021-06-27 19:34:55 -0400

    BuildTools workflow broken, fix later

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,11 +3,11 @@
 # go to /root/MC/BuildTools directory, get latest version of spigot BuildTools,
 # and run BuildTools to get latest version of spigot server .jar
 cd /root/MC/BuildTools/
-wget -N https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
-java -jar BuildTools.jar --rev 1.17
+#wget -N https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
+#java -jar BuildTools.jar --rev 1.17
 
 # move server .jar to server files directory, overwriting if necessary
-mv -u spigot-1.17.jar ../Spigot-Vanilla/spigot-1.17.jar
+#mv -u spigot-1.17.jar ../Spigot-Vanilla/spigot-1.17.jar
 
 # move to server files directory and run start command
 cd ../Spigot-Vanilla
